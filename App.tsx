c5c13c77f7a294a1a8799c978ab57b675263b604
Gabriel Passos <kooujinproductions@gmail.com>
2022-05-06 02:27:17 -0300

    Added the icon to send a feedback, the icons for each type of feedback and the copyright

--- App.tsx
+++ App.tsx
@@ -1,34 +1,68 @@
+import React, { useCallback, useEffect, useState } from 'react';
+
+import 'react-native-gesture-handler'
+import * as SplashScreen from 'expo-splash-screen'
 import { StatusBar } from 'expo-status-bar';
-import AppLoading from 'expo-app-loading';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+
 import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 
 import { theme } from './src/theme'
-import { Widget } from './src/components/Widget'
+import Widget from './src/components/Widget'
+
 
 export default function App() {
-
+  const [appIsReady, setAppIsReady] = useState(false);
   const [fontsLoaded] = useFonts({
-    Inter_400Regular,
-    Inter_500Medium
-  });
+  Inter_400Regular,
+  Inter_500Medium
+});
 
-  if (!fontsLoaded) {
-    return <AppLoading />;
+useEffect(() => {
+  async function loading() {
+    try {
+      // Keep the splash screen visible while we fetch resources
+      await SplashScreen.preventAutoHideAsync();
+      // Pre-load fonts, make any API calls you need to do here
+      await fontsLoaded
+      // Artificially delay for two seconds to simulate a slow loading
+      // experience. Please remove this if you copy and paste the code!
+      await new Promise(resolve => setTimeout(resolve, 2000));
+    } catch (e) {
+      console.warn(e);
+    } finally {
+      // Tell the application to render
+      setAppIsReady(true);
+    }
   }
 
+  loading();
+}, []);
+
+const onLayoutRootView = useCallback(async () => {
+  if (appIsReady) {
+    await SplashScreen.hideAsync();
+  }
+}, [appIsReady]);
+
+if (!appIsReady) {
+  return null;
+}
   return (
-    <View style={{
-      flex: 1,
-      backgroundColor: theme.colors.background
-    }}
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: theme.colors.background
+      }}
+      onLayout={onLayoutRootView}
+
     >
-      <Widget />
       <StatusBar 
       style="light"
       backgroundColor='transparent'
       translucent
       />
+      <Widget />
     </View>
   );
 }
