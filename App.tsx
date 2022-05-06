7bbe9f63ed731632e5d580ec7e0d7eeba5e5c65a
Gabriel Passos <kooujinproductions@gmail.com>
2022-05-06 00:22:21 -0300

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
+import AppLoading from 'expo-app-loading';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+
+import { theme } from './src/theme'
+import { Widget } from './src/components/Widget'
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{
+      flex: 1,
+      backgroundColor: theme.colors.background
+    }}
+    >
+      <Widget />
+      <StatusBar 
+      style="light"
+      backgroundColor='transparent'
+      translucent
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
