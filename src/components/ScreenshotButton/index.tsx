31c92641a07619621308fc93877a97ab16f9cb94
Gabriel Passos <kooujinproductions@gmail.com>
2022-05-07 01:21:21 -0300

    Added camera button

---
+++ src/components/ScreenshotButton/index.tsx
@@ -0,0 +1,29 @@
+
+import React from 'react';
+import { View, TouchableOpacity, Image } from 'react-native';
+
+import { Trash, Camera } from 'phosphor-react-native';
+
+import { styles } from './styles';
+import { theme } from '../../theme';
+
+interface Props {
+  screenshot: string | null
+  onTakShot(): () => void
+  onRemoveShot(): () => void
+}
+
+export function ScreenshotButton({ screenshot, onTakShot, onRemoveShot }: Props) {
+  return (
+    <TouchableOpacity 
+      style={styles.container}
+      onPress={screenshot ? onRemoveShot : onTakShot}
+    >
+    {
+      screenshot ? <Trash size={22} color={theme.colors.text_secondary} weight="fill" style={styles.removeIcon}/>
+      : <Camera size={24} color={theme.colors.text_secondary} weight="bold"/>
+    }
+
+    </TouchableOpacity>
+  );
+}
